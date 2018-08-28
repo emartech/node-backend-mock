99d465fa307fdf875ae88786e563d5f0d2d24085
stoiet <to.stoiet@gmail.com>
2018-08-28 22:57:18 +0200

    feat(nock-wrapper): support reqheaders settings for request matching

--- src/nock-wrapper.ts
+++ src/nock-wrapper.ts
@@ -1,14 +1,18 @@
+import { matchObjects, map } from './utils';
 import { Interceptor } from './interceptor';
-import { matchObjects } from './utils';
 
 import nock from 'nock';
 
 export { Options } from 'nock';
 
+interface RequestHeaders {
+  [key: string]: (headerValue: string) => boolean;
+}
+
 export class NockWrapper {
 
   public registerInterceptor({ host, settings, requestOptions, responseOptions }: Interceptor): void {
-    nock(host, settings)
+    nock(host, { ...settings, reqheaders: this.getRequestHeaders(requestOptions.headers) })
       .intercept(requestOptions.path, requestOptions.method, matchObjects(requestOptions.body))
       .query(matchObjects(requestOptions.query))
       .times(responseOptions.repeat)
@@ -22,6 +26,10 @@ export class NockWrapper {
   public getPendingInterceptors(): string[] {
     if (nock.isDone()) return [];
     return nock.pendingMocks();
+  }
+
+  private getRequestHeaders(headers: object): RequestHeaders {
+    return map(headers, (expectedValue: string) => (value: string) => expectedValue === value);
   }
 
 }
